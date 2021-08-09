c599d7b16e6afee88cb11d3f4aa44d44cb7457f4
stormer3428 <azertyuiopcharles@gmail.com>
2021-08-09 15:38:18 +0200

    Added more functionality to the command "modules"

--- src/fr/stormer3428/demi/module/commands/Modules.java
+++ src/fr/stormer3428/demi/module/commands/Modules.java
@@ -7,6 +7,7 @@ import fr.stormer3428.demi.CommandModule;
 import fr.stormer3428.demi.Demi;
 import fr.stormer3428.demi.Module;
 import fr.stormer3428.demi.DemiCommandReceiveEvent;
+import fr.stormer3428.demi.MixedOutput;
 
 public class Modules extends CommandModule{
 
@@ -16,7 +17,7 @@ public class Modules extends CommandModule{
 		aliases.add("mdls");
 		aliases.add("md");
 		aliases.add("m");
-		
+
 		if(initialConfigIOCreation()) return;
 		OUTPUT.warning("Disabling module to prevent errors");
 		Demi.disableModule(this);
@@ -24,13 +25,28 @@ public class Modules extends CommandModule{
 
 	@Override
 	protected void runCommand(DemiCommandReceiveEvent event) {
-		String str = "Currently loaded modules : \n";
-		boolean first = true;
-		for(Module module : Demi.i.getActiveModules()) {
-			str = str + (first ? "" : ", ") + module.getName();
-			if(first) first = false;
+		ArrayList<String> args = event.getArgs();
+		MixedOutput OUTPUT = event.getOutput();
+
+		if(args.isEmpty()) {
+			String str = "Currently loaded modules : \n";
+			boolean first = true;
+			for(Module module : Demi.i.getActiveModules()) {
+				str = str + (first ? "" : ", ") + module.getName();
+				if(first) first = false;
+			}
+			OUTPUT.command(str);
+			return;
 		}
-		OUTPUT.command(str);
+		String subCommand = args.remove(0);
+		for(Module module : Demi.i.getActiveModules()) {
+			if(module.getName().equalsIgnoreCase(subCommand) || (module instanceof CommandModule && ((CommandModule)module).getAliases().contains(subCommand.toLowerCase()) ) || subCommand.equalsIgnoreCase("description") || subCommand.equalsIgnoreCase("desc") || subCommand.equalsIgnoreCase("d")) {
+				OUTPUT.command("Description of module " + module.getName() + " :");
+				OUTPUT.command(module.getDescription());
+				return;
+			}
+		}
+		OUTPUT.error("No module with such name");
 	}
 
 	@Override
