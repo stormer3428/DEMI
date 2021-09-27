588b1ac52ed8eae7fbc0bbc33c0daf85390a3966
stormer3428 <azertyuiopcharles@gmail.com>
2021-09-27 11:41:55 +0200

    Finished Banname

--- src/fr/stormer3428/demi/module/commands/BanName.java
+++ src/fr/stormer3428/demi/module/commands/BanName.java
@@ -1,14 +1,21 @@
 package fr.stormer3428.demi.module.commands;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.function.Consumer;
 
 import fr.stormer3428.demi.CommandModule;
 import fr.stormer3428.demi.Demi;
 import fr.stormer3428.demi.DemiCommandReceiveEvent;
 import fr.stormer3428.demi.MixedOutput;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
 
 public class BanName extends CommandModule{
 
+	private HashMap<Long, List<Long>> query = new HashMap<>();
+	
 	public BanName() {
 		super("BanName");
 
@@ -25,6 +32,10 @@ public class BanName extends CommandModule{
 
 	@Override
 	protected void runCommand(DemiCommandReceiveEvent event) {
+		long senderId = -1;
+		if(event.getMessageReceivedEvent() != null) {
+			senderId = event.getMessageReceivedEvent().getMember().getIdLong();
+		}
 		List<String> args = event.getArgs();
 		MixedOutput OUTPUT = event.getOutput();
 		if(args.size() == 0) {
@@ -37,16 +48,38 @@ public class BanName extends CommandModule{
 				OUTPUT.command("i need a name to search members with");
 				return;
 			}
-			
-			//TODO
+			String match = args.remove(0);
+			Guild guild = Demi.jda.getGuildById(Demi.i.getServerID());
+			OUTPUT.command("Loading members for process...");
+			guild.loadMembers().onSuccess(successfullLoadConsumer(OUTPUT, match, senderId)).onError(failedLoadConsumer(OUTPUT));
 			return;
 		}
 
 		if(type.equalsIgnoreCase("-confirm") || type.equalsIgnoreCase("-c")) {
-			//TODO
+			banList(OUTPUT, senderId);
 			return;
 		}
 		OUTPUT.command("there's only two valid argument, -query and -confirm, and " + type + " isn't one of them");
+	}
+
+	private void banList(MixedOutput OUTPUT, long senderId) {
+		if(!query.containsKey(senderId)) {
+			OUTPUT.command("ERROR : you do not have any stored queries");
+			return;
+		}
+		List<Long> list = query.remove(senderId);
+		Guild guild = Demi.jda.getGuildById(Demi.i.getServerID());
+		for(Long id : list) {
+			Member member = guild.retrieveMemberById(id).complete();
+			OUTPUT.command("Banned " + member.getEffectiveName());
+			member.ban(14).complete();
+		}
+	}
+
+	private Consumer<? super Throwable> failedLoadConsumer(MixedOutput OUTPUT) {
+		return new Consumer<Throwable>() {@Override public void accept(Throwable threw) {
+			OUTPUT.command("An error was thrown during loading, likely a timeout");
+		}};
 	}
 
 	@Override
@@ -54,5 +87,34 @@ public class BanName extends CommandModule{
 		return "Usage : " + getName() + " <-query; -confirm>";
 	}
 
-
+	private Consumer<? super List<Member>> successfullLoadConsumer(MixedOutput OUTPUT, String match, Long senderId) {return new Consumer<List<Member>>() {@Override public void accept(List<Member> members) {
+		OUTPUT.command("Finished loading members, processing...");
+		List<Member> matchs = new ArrayList<>();
+		List<Long> matchsId = new ArrayList<>();
+		for(Member member : members) {try {
+			if(!member.getEffectiveName().toLowerCase().contains(match) /*&& !member.getUser().getName().toLowerCase().contains(match)*/) continue;
+			matchs.add(member);
+		}catch (Exception e) {}}
+		OUTPUT.command("Found " + matchs.size() + " matches.");
+		String message = "";
+		for(Member member : matchs) {
+			matchsId.add(member.getIdLong());
+			if(message.length() + member.getEffectiveName().length() > 1000) {
+				OUTPUT.command(message);
+				message = "";
+			}
+			message = message + member.getEffectiveName() + "\n";
+		}
+		if(!message.isEmpty()) {
+			OUTPUT.command(message);
+		}
+		query.put(senderId, matchsId);
+	}};}
 }
+
+
+
+
+
+
+
