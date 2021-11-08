8233005d84619023514d9648369140b1b8df6e8b
stormer3428 <azertyuiopcharles@gmail.com>
2021-11-08 16:58:09 +0100

    made it so CAT will say an error whenever one occurs

--- src/fr/stormer3428/demi/module/directoryHandler/CAT.java
+++ src/fr/stormer3428/demi/module/directoryHandler/CAT.java
@@ -14,10 +14,10 @@ import fr.stormer3428.demi.Module;
 public class CAT extends CommandModule{
 
 	private CD CD;
-	
+
 	public CAT() {
 		super("CAT");
-		
+
 		if(initialConfigIOCreation()) return;
 		this.OUTPUT.warning("Disabling module to prevent errors");
 		Demi.disableModule(this);
@@ -29,7 +29,7 @@ public class CAT extends CommandModule{
 		dependencies.add("CD");		
 		return dependencies;
 	}
-	
+
 	@Override
 	public void onEnable() {
 		super.onEnable();
@@ -46,15 +46,15 @@ public class CAT extends CommandModule{
 			return;
 		}
 	}
-	
+
 	@Override
 	protected void runCommand(DemiCommandReceiveEvent event) {
 		Long WDid = event.getMessageReceivedEvent() == null ? 0 : event.getMessageReceivedEvent().getAuthor().getIdLong();
 		MixedOutput OUTPUT = event.getOutput();
-		
+
 		String path = "";
 		for(String s : event.getArgs()) path = path + (path.equals("")?"":" ") + s;
-		
+
 		File file = CD.getFileFromWD(path, WDid);
 		if(file == null) {
 			OUTPUT.command("Outside of root");
@@ -64,21 +64,29 @@ public class CAT extends CommandModule{
 			OUTPUT.command("No file with suche name ("+path+")");
 			return;
 		}
-		if(!file.isDirectory()) {
+		if(file.isDirectory()) {
 			OUTPUT.command("Is directory ("+path+")");
 			return;
 		}
-		
-		IO io = new IO(file, new ArrayList<>(), this.PRINT_STACK_TRACE);
+
+
 		String content = "";
 		String token = Demi.i.discordBotToken();
-		for(String s : io.getAllRaw()) {
+		IO io = new IO(file, new ArrayList<>(), true);
+		io.setPrintStackTrace(false);
+		io.destroy();
+		ArrayList<String> all = io.getAllRaw(false);
+		if(all == null) {
+			OUTPUT.command("Unable to read file ("+path+")");
+			return;
+		}
+
+		for(String s : all) {
 			if(s.contains(token)) s = "[REDACTED]";
-			if(content.length() + s.length() > 1950) break;
-			content = content + s;
+			if(content.length() + s.length() > 1990) break;
+			content = content + (content.equals("")?"":"\n") + s;
 		}
 		OUTPUT.command(content);
-		io.destroy();
 	}
 
 	@Override
