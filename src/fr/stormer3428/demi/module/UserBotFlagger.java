75a18cdf0777e005fcd46716ddc5c8f1b27c7cf6
stormer3428 <azertyuiopcharles@gmail.com>
2023-02-20 08:44:22 +0100

    Updated userbotflagger

--- src/fr/stormer3428/demi/module/UserBotFlagger.java
+++ src/fr/stormer3428/demi/module/UserBotFlagger.java
@@ -5,8 +5,10 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import fr.stormer3428.demi.Demi;
+import fr.stormer3428.demi.IO;
 import fr.stormer3428.demi.Key;
 import fr.stormer3428.demi.MixedOutput;
 import fr.stormer3428.demi.Module;
@@ -26,11 +28,15 @@ public class UserBotFlagger extends Module{
 	private HashMap<Long, Message> loggingMessageMap = new HashMap<>();
 	private HashMap<Long, List<Message>> lastMessagesIDsMap = new HashMap<>();
 
+	private ArrayList<Pattern> triggerRegexes = new ArrayList<>();
+	private IO TRIGGER_REGEXES_DATABASE;
+
 	private static enum Mode{
 		Ping,
 		Autoban,
-		AutobanAtEveryone,
+		AutobanTriggerWord,
 		Dual,
+		DualTriggerWord,
 		Invalid;
 
 		public static Mode getMode(String name) {
@@ -59,12 +65,13 @@ public class UserBotFlagger extends Module{
 		CONFIG_KEYS.add(new Key("blackListedUsers", "[]",
 				"A list of user ids the module should never flag"));
 
-		CONFIG_KEYS.add(new Key("mode", "autobanateveryone",
+		CONFIG_KEYS.add(new Key("mode", "autobanTriggerWord",
 				"The mode of the module : \n"
 						+ "//ping : Pings the roles in rolesToPing\n"
 						+ "//autoBan : Automatically bans the flagged members\n"
-						+ "//autoban : Automatically bans the flagged members if the message contains \"@everyone\"\n"
-						+ "//dual : Automatically ban and ping roles"));
+						+ "//autobanTriggerWord : Automatically bans the flagged members if the message matches a trigger regex"
+						+ "//dual : Automatically ban and ping roles"
+						+ "//dualTriggerWords : Automatically ping roles and bans if the message matches a trigger regex"));
 
 		CONFIG_KEYS.add(new Key("rolesToPing", "[]",
 				"A list of roles to ping whenever the module flags a bot"));
@@ -87,6 +94,32 @@ public class UserBotFlagger extends Module{
 			for(Mode mode : Mode.values()) OUTPUT.error("- " + mode.toString());
 			OUTPUT.error("Disabling module to prevent errors...");
 			Demi.disableModule(this);
+		}
+		triggerRegexes.clear();
+		if(mode == Mode.AutobanTriggerWord || mode == Mode.DualTriggerWord) {
+			TRIGGER_REGEXES_DATABASE = new IO(new File("userbotflaggertriggerregexes.conf"), new ArrayList<>(), true, IO.defaultHeaders, ""
+					+ "// This file is meant to host all trigger regexes for the userbotflagger module\n"
+					+ "\n// You have to put here a regex per line"
+					+ "\n// /!\\ IMPORTANT /!\\"
+					+ "\n// The regex has to  match the ENTIRE message! not just one part"
+					+ "\n// For example, you wat @everyone to be a trigger word, \"@everyone\" will not work unless the message is composed strictly and only of @everyone"
+					+ "\n// for it to match any message CONTAINING @everyone, it would look like this \"/.*@everyone.*/s\""
+					+ "\n// And remember, ONE regex per line, if you want a multiline regex, just use \\n"
+					+ "\n// Here's a nice bewsite to test out regexes on : 'https://regex101.com'"
+					+ "\n"
+					+ "\n.*@everyone.*"
+					+ "\n.*discord\\.gg.*"
+					+ "\n.*discord\\.com.*");
+
+			for(String s : TRIGGER_REGEXES_DATABASE.getAllRaw(true)) {
+				if(s.isBlank() || s.isEmpty()) continue;
+				Pattern pattern = Pattern.compile(s, Pattern.DOTALL);
+				triggerRegexes.add(pattern);
+			}
+			
+			OUTPUT.info("Regex registered");
+			for(Pattern s : triggerRegexes) OUTPUT.info(s.pattern());
+
 		}
 		this.OUTPUT.trace("mode : " + this.mode.toString(), this.PRINT_STACK_TRACE);
 
@@ -147,11 +180,11 @@ public class UserBotFlagger extends Module{
 			lastMessagesIDsMap.put(id, new ArrayList<>());
 		}
 		if(!lastMessagesAmountMap.containsKey(id)) lastMessagesAmountMap.put(id, 0);
-		
+
 		//available variables : member roles id message
 
 		lastMessagesChannelMap.put(id, event.getChannel().getIdLong());
-		
+
 
 		String lastMessage = lastMessagesMap.get(id);
 		if(lastMessage.equals(message)) {
@@ -173,10 +206,10 @@ public class UserBotFlagger extends Module{
 		long id = member.getIdLong();
 		String name = member.getEffectiveName();
 		String lastMessage = lastMessagesMap.get(id);
-		
+
 		EmbedBuilder builder = new EmbedBuilder();
 		List<String> embedReplacement = new ArrayList<>();
-		
+
 		builder.setAuthor("User bot flagger");
 		builder.setTitle("Flagged member " + member.getEffectiveName());
 		builder.setThumbnail(member.getUser().getEffectiveAvatarUrl());
@@ -184,10 +217,21 @@ public class UserBotFlagger extends Module{
 		builder.addField("Flagged member " + name + " as a user bot", member.getAsMention(), false);
 		//LOG.action("Flagged member " + name + "(" + member.getAsMention() + ") as a user bot");
 		embedReplacement.add("Flagged member " + member.getEffectiveName() + "("+member.getId()+") as an user bot");
-		
-		
+
+		boolean containstTriggerWord = false;
+		if(mode == Mode.AutobanTriggerWord || mode == Mode.DualTriggerWord) {
+			for(Pattern regex : triggerRegexes) {
+				OUTPUT.info("checking match for : \"" + regex + "\"");
+				if(regex.matcher(lastMessage).matches()) {
+					OUTPUT.info("Match found!");
+					containstTriggerWord = true;
+					break;
+				}
+			}
+		}
+
 		String status = "Waiting";
-		if(mode == Mode.Autoban ||	mode == Mode.Dual || (mode == Mode.AutobanAtEveryone && lastMessage.contains("@everyone"))) {
+		if(mode == Mode.Autoban ||	mode == Mode.Dual || ((mode == Mode.AutobanTriggerWord || mode == Mode.DualTriggerWord) && containstTriggerWord)) {
 			//LOG.ok("Banned member <@" + member.getIdLong() + "> (" + name + ")");
 			status = "Autobanned";
 			embedReplacement.add("User was autobanned");
@@ -201,7 +245,7 @@ public class UserBotFlagger extends Module{
 			status = "Pinging Moderators for manual decision";
 			builder.addField("AutoPing: ", ping, false);	
 		}
-		
+
 		builder.addField("Status: ", status, false);
 		builder.setDescription(lastMessage);
 		//LOG.info("Message that triggered the flag : \n" + lastMessage);
@@ -214,7 +258,7 @@ public class UserBotFlagger extends Module{
 		}
 		//LOG.info("deleted " + size + " message(s)");
 		builder.addField("deleted " + lastMessagesAmountMap.get(id) + " message" + (size > 1 ? "s" : ""), "", false);
-		
+
 		Message m = null;
 		if(loggingMessageMap.containsKey(id)) {
 			m = loggingMessageMap.get(id).editMessage(builder.build()).complete();
